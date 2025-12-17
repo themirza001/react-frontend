d888158be8b2123e379194033cd35366e0155786
themirza001 <themirza001@gmail.com>
2025-12-17 14:08:25 +0530

    check 4

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "s3_bucket_name" {
   value = aws_s3_bucket.react_bucket.bucket
 }
 
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.cdn.id
+}
+
 output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.cdn.domain_name
 }
